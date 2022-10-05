ccfe12cfa2da6f28e7fd27593c369fa549656592
Carlos Zapata <czapata@sproutai.ad>
2022-10-04 21:08:39 -0500

    first commit

---
+++ Ejercicio_1.py
@@ -0,0 +1,16 @@
+# Decoración de inicio
+print("--------------------------------")
+print("Ejercicio 1: Calcular distancia.")
+print("--------------------------------")
+
+# Entradas:
+print("Ingrese la velocidad y el tiempo de la unidad móvil:")
+# Convertir la entrada a un entero:
+V = float(input("Velocidad:"))
+T = int(input("Tiempo:"))
+
+#Proceso:
+D = V * T
+
+# Salida:
+print(D)
---
+++ Scrapbook.md
@@ -0,0 +1,12 @@
+Ejercicio 1:
+Se Se desea calcular la distancia recorrida (m) por un móvil que tiene velocidad constante (m/s) durante un tiempo t (s), considerar que es un MRU (Movimiento Rectilíneo Uniforme)
+
+Variables
+    - Distancia (m) ?? - Incógnita
+    - Velocidad constante (m/s)
+    - Tiempo (s)
+    - Movimiento: es MRU:
+        V = d/t
+        d = V * t
+
+
