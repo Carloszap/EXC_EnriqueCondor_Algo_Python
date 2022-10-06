2bd4045138edc625feb843919a12014fa9ca1829
Carlos Zapata <czapata@sproutai.ad>
2022-10-05 22:27:00 -0500

    Added exec no. 4

---
+++ Ejercicio_4.py
@@ -0,0 +1,25 @@
+# Decoración de inicio
+from zmq import PEER
+
+
+print("-----------------------------------------------------------------------")
+print("Ejercicio 4: Calcular puntaje total del club ABC en el torneo apertura.")
+print("-----------------------------------------------------------------------")
+
+# Datos de entrada:
+print("Por favor ingrese el número de partidos ganados por el club: ")
+PG = int(input())
+print("Por favor ingrese el número de partidos perdidos por el club: ")
+PP = int(input())
+print("Por favor ingrese el número de partidos empatados por el club: ")
+PE = int(input())
+
+# Cálculo del puntaje:
+PPG = PG * 3
+PPP = PP * 1
+PPE = PE * 0
+
+PF = PPG + PPP + PPE
+
+#Datos de salida:
+print("El puntaje final del club ABC en el torneo apertura es ", PF)
